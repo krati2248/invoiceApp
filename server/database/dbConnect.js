3dda326f7942e1df3350fb3b92bc6c17ee34d83d
krati2248 <184264500+krati2248@users.noreply.github.com>
2026-02-15 15:20:03 +0530

    Move MONGO_URL declaration below dotenv config

--- server/database/dbConnect.js
+++ server/database/dbConnect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose'); 
-const url = process.env.MONGO_URL;
 const dotenv = require('dotenv');
 dotenv.config();
+const url = process.env.MONGO_URL; 
 
 const dbConnect = async () =>
 {
